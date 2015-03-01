83bf757ebca6760ed4053308da04bfb3404c6767
Chris Weathers <chris@connectcommunity.com>
2015-03-01 14:00:31 -0600

    Added images to home page.

--- home.php
+++ home.php
@@ -7,10 +7,11 @@ get_header();
 <div class="container" style="background-color:red;padding:40px;">
     <div class="row">
         <div class="col-xs-12 roll-call">
-            <h2>Roll Call</h2>
-
+            <img src="images/roll-call.gif" height="150px" width="150px" alt="Roll Call" />
+            <br/>
             <form  method= "post" enctype ="multipart/form-data" action = "" >
-                Attach Photo/Video To Your Post &nbsp;
+                <img src="images/image-icon.png" height="30px" width="30px" alt="Photos/Video" />
+                <strong>Attach Photo/Video To Your Post</strong> &nbsp;
                 <input type= "file" name = "flBulletinPhoto" id = "flBulletinPhoto"  />
             <input type="text" name="post" id="post" class="input-style" placeholder="Share Your Talent"/>
 
