ef175ad6488c25b0d63c5d446ba2dcd68539d2ff
Sarah Keefe <sarahjanekeefe@gmail.com>
2015-04-02 15:20:17 -0500

    adding delete comment feature to manage_posts.php

--- manage_post.php
+++ manage_post.php
@@ -308,6 +308,12 @@ if (isset($_POST['Delete']) && $_POST['Delete'] == "Delete") {
     mysql_query($sql) or die (mysql_error());
 
 }
+if (isset($_POST['DeleteComment']) && $_POST['DeleteComment'] == "Delete") {
+    $commentID = $_POST['commentID'];
+    $sql = "Update PostComments SET IsDeleted = '1' WHERE ID = $commentID";
+    mysql_query($sql) or die (mysql_error());
+
+}
 ?>
 
 <style>
@@ -541,6 +547,7 @@ if (isset($_POST['Delete']) && $_POST['Delete'] == "Delete") {
                         PostComments.Post_ID = $postID
                         AND Members.ID = Profile.Member_ID
                         And Members.ID = PostComments.Member_ID
+                        And PostComments.IsDeleted = 0
                         Group By PostComments.ID
                         Order By PostComments.ID ASC LIMIT 3 ";
 
@@ -552,10 +559,20 @@ if (isset($_POST['Delete']) && $_POST['Delete'] == "Delete") {
                     while ($rows3 = mysql_fetch_assoc($result3)) {
                         $comment = $rows3['PostComment'];
                         $profilePhoto = $rows3['ProfilePhoto'];
-
+                        $commentID = $rows3['PostCommentID'];
                         echo '<div class="comment-row">';
 
-                        echo '<div class="user-icon"><img src = "' . $mediaPath . $profilePhoto . '" height = "50" width = "50" style = "border:1px solid black" class ="enlarge-onhover img-responsive" />&nbsp;</td><td valign = "top"><b>' . $rows3['FirstName'] . ' ' . $rows3['LastName'] . '</b>&nbsp;&nbsp;' . nl2br($comment) . '</div>';
+                        echo '<div class="user-icon"><img src = "' . $mediaPath . $profilePhoto . '" height = "50" width = "50" style = "border:1px solid black" class ="enlarge-onhover img-responsive" /><div class="user-name">' . $rows3['FirstName'] . ' ' . $rows3['LastName'] . '</div></div><div class="comment-content">' . nl2br($comment) . '</div>';
+
+                        //<!--DELETE BUTTON ------------------>
+                        echo '<div class="comment-delete">';
+                        echo '<form action="" method="post" onsubmit="return confirm(\'Do you really want to delete this comment?\')">';
+                        echo '<input type="hidden" name="commentID" id="commentID" value="' .  $commentID . '" />';
+                        echo '<input type ="submit" name="DeleteComment" id="DeleteComment" value="Delete" class="deleteButton" />';
+                        echo '</form>';
+                        echo '</div>';
+                        //<!------------------------------------->
+
                         echo '</div>';
                     }
                     echo '</div>';
@@ -580,6 +597,7 @@ if (isset($_POST['Delete']) && $_POST['Delete'] == "Delete") {
                         PostComments.Post_ID = $postID
                         And Members.ID = PostComments.Member_ID
                         And Members.ID = Profile.Member_ID
+                        And PostComments.IsDeleted = 0
                         Group By PostComments.ID
                         Order By PostComments.ID ASC LIMIT 3, 100 ";
 
@@ -599,11 +617,22 @@ if (isset($_POST['Delete']) && $_POST['Delete'] == "Delete") {
                     while ($rows4 = mysql_fetch_assoc($result4)) {
                         $comment = $rows4['PostComment'];
                         $profilePhoto = $rows4['ProfilePhoto'];
+                        $commentID = $rows4['PostCommentID'];
+
+                        echo '<div class="comment-row">';
 
                         echo '<div class="user-icon">';
-                        echo '<img src = "' . $mediaPath . $profilePhoto . '" height = "50" width = "50" style = "border:1px solid black" class ="enlarge-onhover img-responsive" />&nbsp;</td><td valign = "top"><b>' . $rows4['FirstName'] . $rows['LastName'] . '</b>&nbsp;&nbsp;' . nl2br($comment) . '</span>';
+                        echo '<img src = "' . $mediaPath . $profilePhoto . '" height = "50" width = "50" style = "border:1px solid black" class ="enlarge-onhover img-responsive" /><div class="user-name">' . $rows4['FirstName'] . $rows['LastName'] . '</div></div><div class="comment-content">' . nl2br($comment) . '</div>';
 
-                        echo '</td></tr>';
+                        //<!--DELETE BUTTON ------------------>
+                        echo '<div class="comment-delete">';
+                        echo '<form action="" method="post" onsubmit="return confirm(\'Do you really want to delete this comment?\')">';
+                        echo '<input type="hidden" name="commentID" id="commentID" value="' .  $commentID . '" />';
+                        echo '<input type ="submit" name="DeleteComment" id="DeleteComment" value="Delete" class="deleteButton" />';
+                        echo '</form>';
+                        echo '</div>';
+                        //<!------------------------------------->
+                        echo '</div>';
 
                     }
                     echo '</div>';
