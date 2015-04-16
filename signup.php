f3564a4f46a660fdd1f434133b7a1ca44868fd35
Chris Weathers <chris@rapportbook.com>
2015-04-15 20:02:02 -0500

    Refactored Sign Up to handle FB data.

--- signup.php
+++ signup.php
@@ -15,49 +15,76 @@ $day = $_POST['ddDay'];
 $year = $_POST['ddYear'];
 $username = $_POST['username'];
 $pass = $_POST['password'];
+$fb_token = $_POST['fb_token'];
+$fb_id = $_POST['fb_id'];
+
+if($gender=='') $gender = (($_POST['gender']=='male')?1:2);
 
 $dob = $year . '-' . $month . '-' . $day;
 
 
-$sql = "SELECT Email FROM Members WHERE Email = '$email'";
+$sql = "SELECT * FROM Members WHERE Email = '$email'";
 $result = mysql_query($sql) or die(mysql_error());
 
 if (mysql_num_rows($result) > 0) {
-    echo '<script>alert("You already have an profile, please login");location = "index.php"</script>';
-    exit;
+
+    if($fb_id!='') {
+
+        $rows = mysql_fetch_assoc($result);
+        session_start();
+
+        //die(print_r($rows));
+
+        $_SESSION['ID'] = $rows['ID'];
+
+        setcookie("ID", $rows['ID'], time() + (10 * 365 * 24 * 60 * 60)); // set cookie for 10 years
+
+        //update last login
+        $id = $rows['ID'];
+        $date = date('Y-m-d H:i:s');
+        $sql2 = "UPDATE Members SET LastLogin = '$date' WHERE ID = '$id' ";
+        mysql_query($sql2) or die(mysql_error());
+
+        exit;
+
+    }
+    else{
+        echo '<script>alert("You already have an profile, please login");location = "index.php"</script>';
+        exit;
+    }
 }
 
-    $sql = "INSERT INTO Members (FirstName, LastName, Email, Gender, DOB, Username, Password, SignupDate, IsSuspended, EmailActive, LastLogin)
-    Values ('$fName', '$lName', '$email', '$gender', '$dob', '$username', '".md5($pass)."', CURRENT_DATE(), 0, 1, CURRENT_DATE())";
-    $result = mysql_query($sql) or die(mysql_error());
+$sql = "INSERT INTO Members (FirstName, LastName, Email, Gender, DOB, Username, Password, SignupDate,     IsSuspended, EmailActive, LastLogin,        fb_token,   fb_id)
+    Values ('$fName', '$lName', '$email', '$gender', '$dob', '$username', '".md5($pass)."', CURRENT_DATE(), 0,              1,       CURRENT_DATE(),'$fb_token','$fb_id')";
+$result = mysql_query($sql) or die(mysql_error());
 
-    $ID = mysql_insert_id();
+$ID = mysql_insert_id();
 
-    $sql = "SELECT * FROM Members WHERE ID = $ID ";
-    $result = mysql_query($sql) or die(mysql_error());
-    $rows = mysql_fetch_assoc($result);
+$sql = "SELECT * FROM Members WHERE ID = $ID ";
+$result = mysql_query($sql) or die(mysql_error());
+$rows = mysql_fetch_assoc($result);
 
-    /*Set two session variable which will be needed throughout the program */
-    session_start();
-    $_SESSION['ID'] = $rows['ID'];
-    setcookie("ID", $rows['ID'], time() + (10 * 365 * 24 * 60 * 60)); // set cookie for 10 years
+/*Set two session variable which will be needed throughout the program */
+session_start();
+$_SESSION['ID'] = $rows['ID'];
+setcookie("ID", $rows['ID'], time() + (10 * 365 * 24 * 60 * 60)); // set cookie for 10 years
 
-    //sign up date
+//sign up date
 
-    $date = date('Y-m-d H:i:s');
-    $sql = "UPDATE Members SET SignupDate = '$date' WHERE ID = '$ID' ";
-    $result = mysql_query($sql) or die(mysql_error());
+$date = date('Y-m-d H:i:s');
+$sql = "UPDATE Members SET SignupDate = '$date' WHERE ID = '$ID' ";
+$result = mysql_query($sql) or die(mysql_error());
 
-    // insert default profile pic into profile table
-    $sql = "INSERT INTO Profile (Member_ID, ProfilePhoto,     ProfileVideo,      HomeState, HomeCity, CurrentCity, CurrentState, Interests, Books, Movies, Food, Dislikes, Plan) Values
+// insert default profile pic into profile table
+$sql = "INSERT INTO Profile (Member_ID, ProfilePhoto,     ProfileVideo,      HomeState, HomeCity, CurrentCity, CurrentState, Interests, Books, Movies, Food, Dislikes, Plan) Values
                                 ('$ID', 'default_photo.png', 'default_video.png', '', '', '', '', '', '', '', '', '', '')    ";
-    $result = mysql_query($sql) or die(mysql_error());
+$result = mysql_query($sql) or die(mysql_error());
 
 
-    // Send out sign up email
-    $toId = $rows['ID'];
-    build_and_send_email(1,$ID, 3, null);
+// Send out sign up email
+$toId = $rows['ID'];
+build_and_send_email(1,$ID, 3, null);
 
-    echo '<script>alert("Your profile was successfully set up");location = "home.php"</script>';
+echo '<script>alert("Your profile was successfully set up");location = "home.php"</script>';
 
 ?>
