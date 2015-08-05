73ca9f1cac97320c3b383047e1cbff6860c46b77
Chris Weathers <chris@rapportbook.com>
2015-08-05 14:43:25 -0500

    Only allow Rapportbook to be viewed in Chrome & Safari

--- connect.php
+++ connect.php
@@ -1,4 +1,23 @@
 <?php
+
+if(strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== FALSE) {
+    echo "<script>alert('Rapportbook can only be viewed in Chrome or Safari');location='http://google.com'</script>";
+}
+elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Trident') !== FALSE) {
+    //For Supporting IE 11
+    echo "<script>alert('Rapportbook can only be viewed in Chrome or Safari');location='http://google.com'</script>";
+}
+elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Firefox') !== FALSE) {
+    echo "<script>alert('Rapportbook can only be viewed in Chrome or Safari');location='http://google.com'</script>";
+}
+elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Opera Mini') !== FALSE) {
+    echo "<script>alert('Rapportbook can only be viewed in Chrome or Safari');location='http://google.com'</script>";
+}
+elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Opera') !== FALSE) {
+    echo "<script>alert('Rapportbook can only be viewed in Chrome or Safari');location='http://google.com'</script>";
+}
+
+
 error_reporting(E_ERROR);
 $url = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 $localhost = 'localhost';
