e3641670b6caa0cb58673f61ce48738aa1eb8ebb
Chris Weathers <chris@rapportbook.com>
2015-08-18 07:45:22 -0500

    Fixed typos in learn more services descriptions.

--- learn_more.php
+++ learn_more.php
@@ -22,11 +22,11 @@ get_header()
             <a href="/home.php">Back to Roll Call</a>
             <?php } ?>
 
-            <h4>Rapportbook allows people to find the services they are looking for.</h4>
+            <h4>Rapportbook helps people find the servies they need.</h4>
 
 
 
-                <img src="<?php echo $imagesPath ?>services.jpg" class="how-it-works-img"/>
+                <img src="<?php echo $imagesPath ?>services.jpg" />
 
 
                 <p>
@@ -34,18 +34,18 @@ get_header()
                     When you sign up, you select "I am looking for a service".
                     Once you sign up, you simply post what you are looking for from
                     the drop down along with text that describes what you are looking for.
-                    You can also add a photo or video to better describe you problem if it helps.
+                    You can also add a photo or video to better describe your problem if it helps.
 
                     <br/>
-                    Once you hit post, a notification via email and text will got out to all service providers
+                    Once you hit post, a notification via email and SMS text will got out to all service providers
                     who focus on the service you need. The service providers will then come back
-                    to Rapportbook and return you message. From there, you and the service provider
+                    to Rapportbook and return your message. From there, you and the service provider
                     will be able to discuss the problem at hand. You can choose to talk to as many
                     services providers as possible before making your decision on who you will work with.
 
                     <br/>
-                    All of your service requests will be stored in your profile a viewable by service
-                    providers once they start a conversation with you. You can delete any of these at any time.
+                    All of your service requests will be stored in your profile and be viewable by service
+                    providers once they start a conversation with you. You can delete any of these requests at any time.
                 </p>
 
                 <p>
@@ -53,10 +53,9 @@ get_header()
                     When you sign up, you select "I am providing a service".
                     Once you sign up, you can scroll through all service requests,
                     even the ones you don't specialize in. But you will receive notifications
-                    whenever their is a post related to you primary focus.
+                    whenever their is a post related to your primary focus.
                     You can then respond to the request. Service providers can also post
-                    a profile video to better explain what their service does. You will also have
-                    a library of videos for service seekers to explore.
+                    a profile video along with about us text to better explain what your service does.
                 </p>
 
     </div>
