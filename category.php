ab54b592adcfa47f654a34956c60990f7caefac0
Chris Weathers <chris@rapportbook.com>
2015-09-17 08:28:11 -0500

    Changed Baby Sitter to Day Care.

--- category.php
+++ category.php
@@ -3,12 +3,12 @@ function category()
 {
 
     echo '<option value = "Automotive">Automotive</option>';
-    echo '<option value = "Baby Sitter">Baby Sitter</option>';
     echo '<option value = "Barber">Barber</option>';
     echo '<option value = "Cleaning">Cleaning</option>';
     echo '<option value = "Computer">Computer</option>';
     echo '<option value = "Contractor">Contractor</option>';
     echo '<option value = "Cosmetology">Cosmetology</option>';
+    echo '<option value = "Day Care">Day Care</option>';
     echo '<option value = "Financial">Financial</option>';
     echo '<option value = "Fitness">Fitness</option>';
     echo '<option value = "Insurance">Insurance</option>';
