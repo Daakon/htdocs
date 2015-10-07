f2a9276ad5565cbe0e9b019a7ad2c757e15e3797
Chris Weathers <chris@rapportbook.com>
2015-10-07 11:23:26 -0500

    Tweaked findUrl to detect urls with only a .com and no http or www

--- findURL.php
+++ findURL.php
@@ -9,6 +9,12 @@ function makeLinks($str)
         // has formatted links don't do anything
         return $str;
     } else {
+        // detect urls with ONLY a .com and NO http and NO www and NO anchr
+        $str = preg_replace_callback('#(\s|^)((https?://)?(\w|-)+(\.[a-z]{2,3})+(\:[0-9]+)?(?:/[^\s]*)?)(?=\s|\b)#is',
+            create_function('$m', 'if (!preg_match("#^(https?://)#", $m[2])) return $m[1]."<a href=\"http://".$m[2]."\" target=\"blank\">".$m[2]."</a>"; else return $m[1]."<a href=\"".$m[2]."\" target=\"blank\">".$m[2]."</a>";'),
+            $str);
+
+        // detect urls WITH a www but NO http and NO anchor
         $str = preg_replace('$(https?://[a-z0-9_./?=&#-]+)(?![^<>]*>)$i', ' <a href="$1" target="_blank">$1</a> ', $str . " ");
         $str = preg_replace('$(www\.[a-z0-9_./?=&#-]+)(?![^<>]*>)$i', '<a target="_blank" href="http://$1"  target="_blank">$1</a> ', $str . " ");
         return $str;
