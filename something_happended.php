b92dddc96e645ca626f279c9802fe4ed65cfffc8
Chris Weathers <chris@rapportbook.com>
2016-01-02 16:44:15 -0600

    Added media page.

---
+++ something_happended.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chrismoney
+ * Date: 1/2/16
+ * Time: 5:37 PM
+ */
+require 'imports.php';
+?>
+
+<img src="/images/oops.jpg" height="100" width="100"/>
+<div style="align:center"><h2>Page Not Found</h2></div>
