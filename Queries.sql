3f261a6b184c4b3796bd86b92b91d12617a5f907
Chris Weathers <chris@rapportbook.com>
2016-05-03 16:23:38 -0500

    Updated Queries to get points for individual member

--- Queries.sql
+++ Queries.sql
@@ -1,18 +1,13 @@
 --Post Query
-Select Post From Posts Where (Member_ID = 1) and (IsDeleted = 0) and (IsRedeemed = 0)
+Select count(Post) as PostCount
+From Posts
+Where (Member_ID = 1) and (IsDeleted = 0) and (IsRedeemed = 0)
 
 --Follower Query
-SELECT Distinct Members.FirstName, Members.LastName, Follows.Followed_ID As ParentFollowedID, (Select count(Followed_ID) From Follows where Followed_ID = ParentFollowedID) As FollowerCount, FollowDate
-FROM Follows,Members
-Where (Members.ID = Follows.Followed_ID)
-order by FollowerCount desc
+SELECT COUNT( Followed_ID ) AS FollowerCount
+FROM Follows
+WHERE Followed_ID =168
+AND IsRedeemed =0
+ORDER BY FollowerCount DESC
 
---Media Likes Query
-SELECT Distinct Members.FirstName,Members.LastName, MediaApprovals.Media_ID As ParentMediaID,
-(Select count(Media_ID) From MediaApprovals where Media_ID = ParentMediaID) As MediaLikes, MediaApproveDate
-FROM Members, MediaApprovals,Media
-Where (Members.ID = Media.Member_ID)
-And (MediaApprovals.Media_ID = Media.ID)
-And (MediaApproveDate Between '2016-04-17' and '2016-04-24')
-order by MediaLikes desc
 
