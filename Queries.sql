adf16a28e5485b28f1482e678676082490e3bf74
Chris Weathers <chris@rapportbook.com>
2016-05-19 22:29:49 -0500

    Updated Queries

--- Queries.sql
+++ Queries.sql
@@ -1,25 +1,18 @@
---Post Query
-Select count(Post) as PostCount
-From Posts
-Where (Member_ID = X) and (IsDeleted = 0) and (IsRedeemed = 0)
-
 --Post Like Query
 Select count(ID) as LikeCount
 From PostApprovals
 Where (Member_ID = X) and (IsRedeemed = 0)
 
 --Follower Query
-SELECT COUNT( Followed_ID ) AS FollowerCount
+/*SELECT COUNT( Followed_ID ) AS FollowerCount
 FROM Follows
 WHERE Followed_ID = X
 AND IsRedeemed =0
-ORDER BY FollowerCount DESC
+ORDER BY FollowerCount DESC*/
 
 
 --Update Redemption Status
-Update Posts Set IsRedeemed = 1 Where IsRedeemed = 0 And Member_ID = X
-
 Update PostApprovals Set IsRedeemed = 1 Where IsRedeemed = 0 And Member_ID = X
 
-Update Follows Set IsRedeemed = 1 Where IsRedeemed = 0 And Followed_ID = X
+--Update Follows Set IsRedeemed = 1 Where IsRedeemed = 0 And Followed_ID = X
 
